502af577e755f457f1db1e69dcac31542723f6fc
kemel <kemelcallisaya@yahoo.de>
2024-09-24 17:58:45 -0400

    Update Size Corrected

--- components/ImageGenerationForm.tsx
+++ components/ImageGenerationForm.tsx
@@ -192,7 +192,7 @@ export default function ImageGenerationForm({ onImageGenerated }: ImageGeneratio
         <Label>Height: {height}px</Label>
         <Slider
           min={64}
-          max={1048}
+          max={2048}
           step={64}
           value={[height]}
           onValueChange={(value) => setHeight(value[0])}
